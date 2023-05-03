785590711c9196d467831f3f6c6a7328e0ab8498
Minecuta Razvan <92296392+MinecutaRazvan@users.noreply.github.com>
2023-05-03 23:09:40 +0300

    Update Header.jsx
    Centrare titlu si pastrarea Main-ului si al Cautarii utilizator in partea dreapta

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,23 +6,21 @@ function Header() {
 
 
     return (
-        <div className='flex bg-gray-700 w-full justify-between items-center p-4 text-white'>
-            <div>
-                <h1 className='text-3xl font-bold'>Health Monitor Web App  </h1>
+        <div className='w-full flex bg-gray-700 justify-center items-center p-4 text-white'>
+           <div className='w-3/4 flex justify-center' style={{ marginLeft: '350px' }}>
+                <h1 className='text-3xl font-bold'>Health Monitor Web App</h1>
             </div>
-            <ul className='flex'>
-                <li className='px-4 cursor-pointer capitalize hover:scale-105 duration-200'>
-                    <Link to="/">Main</Link>
-                </li>
-                <li className='px-4 cursor-pointer capitalize hover:scale-105 duration-200'>
-                    <Link to="/rezultate">Cautare utilizator</Link>
-                </li>
-
-            </ul>
-
-
-
-        </div>
+            <div className='w-1/4 flex justify-end'>
+                <ul className='flex'>
+                    <li className='px-4 cursor-pointer capitalize hover:scale-105 duration-200'>
+                        <Link to="/">Main</Link>
+                    </li>
+                    <li className='px-4 cursor-pointer capitalize hover:scale-105 duration-200'>
+                        <Link to="/rezultate">Cautare utilizator</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>  
     )
 }
 
