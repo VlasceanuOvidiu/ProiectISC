1fe686647728e4b1a878863ae0753a29d58848f5
Minecuta Razvan <92296392+MinecutaRazvan@users.noreply.github.com>
2023-05-03 23:30:36 +0300

    Update Header,jsx
    Am adaugat 2 iconite in stanga si dreapta titlului

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,11 +6,12 @@ function Header() {
 
 
     return (
-        <div className='w-full flex bg-gray-700 justify-center items-center p-4 text-white'>
-           <div className='w-3/4 flex justify-center' style={{ marginLeft: '350px' }}>
-                <h1 className='text-3xl font-bold'>Health Monitor Web App</h1>
-            </div>
-            <div className='w-1/4 flex justify-end'>
+         <div className='flex bg-gray-700 justify-center items-center p-4 text-white'>
+            <div className='w-3/4 flex justify-center' style={{ marginLeft: '200px' }}>
+                 <i className="fas fa-medkit" style={{ fontSize: '24px', width: '24px', height: '24px', color: 'red' }}></i>
+                      <h1 className='text-3xl font-bold'>Health Monitor Web App</h1>
+                   <i className="fas fa-medkit" style={{ fontSize: '24px', width: '24px', height: '24px', color: 'red' }}></i>
+           </div>
                 <ul className='flex'>
                     <li className='px-4 cursor-pointer capitalize hover:scale-105 duration-200'>
                         <Link to="/">Main</Link>
@@ -20,7 +21,6 @@ function Header() {
                     </li>
                 </ul>
             </div>
-        </div>  
     )
 }
 
