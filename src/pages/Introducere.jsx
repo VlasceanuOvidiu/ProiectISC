9d4c5ccce5ffde45177a48b3d3d5ebb12973b391
Minecuta Razvan <92296392+MinecutaRazvan@users.noreply.github.com>
2023-05-03 23:54:09 +0300

    Update Introducere.jsx
    Stilizare pentru input uri ,adaugare radius rosu in momentul cand se face click pe input

--- src/pages/Introducere.jsx
+++ src/pages/Introducere.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { addDoc, collection, serverTimestamp } from 'firebase/firestore'
 import { db } from '../firebase'
+import './introducere.css'
 
 function Introducere() {
 
@@ -28,24 +29,50 @@ function Introducere() {
     };
 
     return (
-        <div className='w-full min-h-screen bg-gray-400 p-12'>
-            <form onSubmit={handleSubmit} className='flex flex-col p-6 items-center justify-center'>
-                <label className="block">
-                    <span className="text-gray-700">Nume:</span>
-                    <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                </label>
-                <label className="block">
-                    <span className="text-gray-700">Email:</span>
-                    <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                </label>
-                <label className="block">
-                    <span className="text-gray-700">Nivel de oxigen:</span>
-                    <input type="number" value={message} onChange={(e) => setMessage(e.target.value)} className="mt-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-                </label>
-                <button className='mt-4 px-4 py-2 bg-red-500 border-black rounded-xl' type="submit">Trimite</button>
-            </form>
+        <div className="w-full min-h-screen bg-gray-400 p-12">
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col p-6 items-center justify-center"
+          >
+            <label className="block mb-4">
+              <span className="text-gray-700 font-bold text-xl mb-2">
+                Nume: <br></br>
+              </span>
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className="input-style"
+              />
+            </label>
+            <label className="block mb-4">
+              <span className="text-gray-700 font-bold text-xl mb-2">
+                Email: <br></br>
+              </span>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="input-style"
+              />
+            </label>
+            <label className="block mb-4">
+              <span className="text-gray-700 font-bold text-xl mb-2">
+                Nivel de oxigen: <br></br>
+              </span>
+              <input
+                type="number"
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                className="input-style"
+              />
+            </label>
+            <button className="mt-4 px-4 py-2 bg-red-500 border-black rounded-xl" type="submit">
+              Trimite
+            </button>
+          </form>
         </div>
-    );
+      );
 }
 
 export default Introducere
