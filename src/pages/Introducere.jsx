8f71ed8203e245cd1ba87dee5bdacf4795d60688
Muresan Vicentiu <65223988+Muresan-Vicentiu@users.noreply.github.com>
2023-05-04 11:29:05 +0300

    Update Introducere.jsx

--- src/pages/Introducere.jsx
+++ src/pages/Introducere.jsx
@@ -77,6 +77,17 @@ function Introducere() {
               Trimite
             </button>
           </form>
+          <div class="container">
+	          <div class="content">
+		          <div class="heart-rate">
+		        	  <svg >
+                          <polyline fill="none" stroke="#000000" stroke-width="3" stroke-miterlimit="10" points="0,45.486 38.514,45.486 44.595,33.324 50.676,45.486 57.771,45.486 62.838,55.622 71.959,9 80.067,63.729 84.122,45.486 97.297,45.486 103.379,40.419 110.473,45.486 150,45.486"/>
+                      </svg>
+                      <div class="fade-in"> </div>
+                      <div class="fade-out"> </div>
+		          </div>
+		      </div>
+	       </div> 
         </div>
       );
 }
