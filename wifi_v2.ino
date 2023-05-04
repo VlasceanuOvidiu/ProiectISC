520f481338f2c903a599e7fb2ab91bbc046877d7
StefanGheorghe <93592765+Fanutzzz@users.noreply.github.com>
2023-05-04 10:26:48 +0300

    Add files via upload

--- wifi_v2.ino
+++ wifi_v2.ino
@@ -1,31 +1,44 @@
-#include <WiFi.h>
 #include <Wire.h>
 #include "MAX30105.h"
-#include "heartRate.h"
-#include <HTTPClient.h>
+#include "spo2_algorithm.h"
+#include <WiFi.h>
+#include <ThingSpeak.h>
 
 MAX30105 particleSensor;
+#define MAX_BRIGHTNESS 255
 
-const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
-byte rates[RATE_SIZE]; //Array of heart rates
-byte rateSpot = 0;
-long lastBeat = 0; //Time at which the last beat occurred
+#if defined(__AVR_ATmega328P__) || defined(__AVR_ATmega168__)
+uint16_t irBuffer[100]; //infrared LED sensor data
+uint16_t redBuffer[100];  //red LED sensor data
+#else
+  uint32_t irBuffer[100]; //infrared LED sensor data
+  uint32_t redBuffer[100];  //red LED sensor data
+#endif
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+byte pulseLED = 11; //Must be on PWM pin
+byte readLED = 13; //Blinks with each data read
 
-float beatsPerMinute;
-int beatAvg;
+long prevMillisSensor = 0;
+int intervalSensor = 1000;
+long prevMillisThingSpeak = 10000;
+int intervalThingSpeak = 15000; // Minimum ThingSpeak write interval is 15 seconds
 
-const char* ssid = "DESKTOP-FAIKH1K 9564";
-const char* password = "4:t02S27";
-const char* serverName = "http://localhost:3000/"; // Your web server address and endpoint
+const char* ssid = "402"; // your network SSID (name)
+const char* password = "faraparola"; // your network password
+const long CHANNEL = 2132916;
+const char *WRITE_API = "CIA2ZBO9VHDVS7BO";
 
-void setup()
-{
-  Serial.begin(115200);
-  Serial.println("Initializing...");
+WiFiClient client;
 
-  // Connect to Wi-Fi network
-  Serial.print("Connecting to ");
-  Serial.println(ssid);
+void setup() {
+  Serial.begin(115200); // initialize serial communication at 115200 bits per second:
+  pinMode(pulseLED, OUTPUT);
+  pinMode(readLED, OUTPUT);
+
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -33,64 +46,94 @@ void setup()
   }
   Serial.println("Connected to WiFi");
 
-  // Initialize sensor
+  ThingSpeak.begin(client);
+  
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
   {
-    Serial.println("MAX30102 was not found. Please check wiring/power. ");
+    Serial.println(F("MAX30105 was not found. Please check wiring/power."));
     while (1);
   }
-  Serial.println("Place your index finger on the sensor with steady pressure.");
 
-  particleSensor.setup(); //Configure sensor with default settings
-  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
+  while (Serial.available() == 0); //wait until user presses a key
+  Serial.read();
+
+  byte ledBrightness = 60; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 411; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
+  particleSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
 }
 
 void loop()
 {
-  long irValue = particleSensor.getIR();
-
-  if (checkForBeat(irValue) == true)
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+  for (byte i = 0 ; i < bufferLength ; i++)
   {
-    //We sensed a beat!
-    long delta = millis() - lastBeat;
-    lastBeat = millis();
-
-    beatsPerMinute = 60 / (delta / 1000.0);
-
-    if (beatsPerMinute < 255 && beatsPerMinute > 20)
+    while (particleSensor.available() == false) //do we have new data?
+      particleSensor.check(); //Check the sensor for new data
+    redBuffer[i] = particleSensor.getRed();
+    irBuffer[i] = particleSensor.getIR();
+    particleSensor.nextSample(); //We're finished with this sample so move to next sample
+    Serial.print(F("red="));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F(", ir="));
+    Serial.println(irBuffer[i], DEC);
+  }
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  while (1)
+  {
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
     {
-      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-      rateSpot %= RATE_SIZE; //Wrap variable
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+    for (byte i = 75; i < 100; i++)
+    {
+      while (particleSensor.available() == false) //do we have new data?
+        particleSensor.check(); //Check the sensor for new data
+      digitalWrite(readLED, !digitalRead(readLED)); //Blink onboard LED with every data read
+      redBuffer[i] = particleSensor.getRed();
+      irBuffer[i] = particleSensor.getIR();
+      particleSensor.nextSample(); //We're finished with this sample so move to next sample
+      Serial.print(F("red="));
+      Serial.print(redBuffer[i], DEC);
+      Serial.print(F(", ir="));
+      Serial.print(irBuffer[i], DEC);
+      Serial.print(F(", HR="));
+      Serial.print(heartRate, DEC);
+      Serial.print(F(", HRvalid="));
+      Serial.print(validHeartRate, DEC);
+      Serial.print(F(", SPO2="));
+      Serial.print(spo2, DEC);
+      Serial.print(F(", SPO2Valid="));
+      Serial.println(validSPO2, DEC);
+    }
 
-      //Take average of readings
-      beatAvg = 0;
-      for (byte x = 0 ; x < RATE_SIZE ; x++)
-        beatAvg += rates[x];
-      beatAvg /= RATE_SIZE;
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
 
-      // Send data to web server
-      if (WiFi.status() == WL_CONNECTED) {
-        HTTPClient http;
-        String data = "beatsPerMinute=" + String(beatsPerMinute) + "&beatAvg=" + String(beatAvg);
-        Serial.println("Sending data: " + data);
 
-        http.begin(serverName);  // Specify the server and endpoint
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded"); // Specify content-type header
-        int httpResponseCode = http.POST(data); // Send HTTP POST request
-        if (httpResponseCode > 0) {
-          Serial.print("HTTP Response code: ");
-          Serial.println(httpResponseCode);
-        }
-        else {
-          Serial.print("Error on HTTP request: ");
-          Serial.println(httpResponseCode);
-        }
-        http.end();
+    if (validSPO2==1 and validHeartRate==1)
+      if (millis() - prevMillisThingSpeak > intervalThingSpeak) {
+
+      // Set the fields with the values
+      ThingSpeak.setField(1, spo2);
+      ThingSpeak.setField(2, heartRate);
+
+      // Write to the ThingSpeak channel
+      int x = ThingSpeak.writeFields(CHANNEL, WRITE_API);
+      if (x == 200) {
+        Serial.println("Channel update successful.");
       }
       else {
-        Serial.println();
-
+        Serial.println("Problem updating channel. HTTP error code " + String(x));
       }
 
-    }}}
+      prevMillisThingSpeak = millis();
+    }
+  }
+}
+
