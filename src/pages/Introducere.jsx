56915e437cad2cc535ec76495ea4237eefc97b33
Muresan Vicentiu <65223988+Muresan-Vicentiu@users.noreply.github.com>
2023-05-07 11:51:37 +0300

    Update Introducere.jsx
    Validari inputuri, field-ul de Puls in timp real arata ca orice alt field de input doar ca nu este editabil si preia valoarea din api-ul de pe thingspeak

--- src/pages/Introducere.jsx
+++ src/pages/Introducere.jsx
@@ -9,7 +9,10 @@ function Introducere() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [message, setMessage] = useState(null);
-
+  const [nameError, setNameError] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [pulse, setPulse] = useState('');
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
@@ -34,59 +37,58 @@ function Introducere() {
       const response = await axios.get(
         'https://api.thingspeak.com/channels/2132916/fields/1/last.json'
       );
-      setMessage(response.data.field1);
+      setPulse(response.data.field1);
     };
     fetchData();
   }, []);
+  
+  const handleNameChange = (e) => {
+    const value = e.target.value;
+    setName(value);
+    const nameRegex = /^[a-zA-Z]+(?: [a-zA-Z]+)?$/;
+    if (!value.match(nameRegex)) {
+      setNameError('Introduceti un nume valid');
+    } else {
+      setNameError('');
+    }
+  };
 
+  const handleEmailChange = (e) => {
+    const value = e.target.value;
+    setEmail(value);
+
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!value.match(emailRegex)) {
+      setEmailError('Introduceti o adresa de email valida');
+    } else {
+      setEmailError('');
+    }
+  };
 
   const isDisabled = name === '' || email === '' || message === '';
 
   return (
-
     <div className="w-full min-h-screen bg-gray-400 p-12">
-
-      <div className="flex flex-col p-6 font-bold items-center justify-center text-2xl">
-        <h1 className='mb-6'>Real time Pulse :</h1>
-        <iframe width="450" height="260" src="https://thingspeak.com/channels/2132916/widgets/643941"></iframe>
-      </div>
-
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col p-6 items-center justify-center"
-      >
+      <form onSubmit={handleSubmit} className="flex flex-col p-6 items-center justify-center">
         <label className="block mb-4">
-          <span className="text-gray-700 font-bold text-xl mb-2">
-            Nume: <br></br>
-          </span>
+          <span className="text-gray-700 font-bold text-xl mb-2">Nume: <br /></span>
+          <input type="text" value={name} onChange={handleNameChange} className="input-style" />
+          {nameError && <p className="text-red-500 text-sm">{nameError}</p>}
+        </label>
+
+        <label className="block mb-4">
+          <span className="text-gray-700 font-bold text-xl mb-2">Email: <br /></span>
+          <input type="email" value={email} onChange={handleEmailChange} className="input-style" />
+          {emailError && <p className="text-red-500 text-sm">{emailError}</p>}
+        </label>
+
+        <label className="block mb-4">
+          <span className='text-gray-700 font-bold text-xl mb-2'>Pulsul in timp real: <br /></span>
           <input
             type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            value={pulse}
+            readOnly={true}
             className="input-style"
-          />
-        </label>
-        <label className="block mb-4">
-          <span className="text-gray-700 font-bold text-xl mb-2">
-            Email: <br></br>
-          </span>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="input-style"
-          />
-        </label>
-        <label className="block mb-4">
-          <span className="text-gray-700 font-bold text-xl mb-2">
-            Nivel de oxigen: <br></br>
-          </span>
-          <input
-            type="number"
-            onChange={(e) => setMessage(e.target.value)}
-            className="input-style"
-            placeholder="Nivel de oxigen"
-            value={message}
           />
         </label>
         <button
